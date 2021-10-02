2c61df29b65334d8a2d6eadc74066e2f7ccfd43d
atom20000 <0362725@mail.ru>
2021-10-02 18:17:55 +0300

    Add output of encoded message

--- Shannon-Fano.py
+++ Shannon-Fano.py
@@ -8,16 +8,17 @@ def symbol_code(arr, sym):
 def Shennon(list_):
     probab_coeff = sum(map(lambda x: x[1], list_))/2
     prom_sum = 0
-    index = 0
     for i in range(len(list_)):
         prom_sum += list_[i][1]
         if prom_sum >= probab_coeff:
             index = i + (abs(prom_sum - probab_coeff) < abs(prom_sum - list_[i][1] - probab_coeff))
             break
-    print(index)
+        
+#Теоретически можно везде сделать +1 к индесу, но это менее эффективно по сравнению с этим спопобом.
+    
     symbol_code(list_[:index],'1')
     symbol_code(list_[index:],'0')
-    print(Code)
+    
     if len(list_[:index])!=1:
         Shennon(list_[:index])
 
@@ -28,5 +29,7 @@ unic_char = sorted(list(set(orig_text)))
 len_text = len(orig_text)
 list_frequ = sorted([(char,round(orig_text.count(char)/len_text, 4)) for char in unic_char],key = lambda x: (-x[1], x[0]))
 Code = {char : '' for char in unic_char}
+print('Список типа: "символ" - "частота появления"\n',list_frequ)
 Shennon(list_frequ)
-print(Code)
+print('Словарь типа: "символ" - "код"\n',Code)
+print('Закодированное сообщение:\n', ''.join([Code[i] for i in orig_text]))
