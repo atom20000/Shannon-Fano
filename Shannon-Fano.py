8145f8f06babcd5696a932a993e81aa6f36e1a70
atom20000 <0362725@mail.ru>
2021-10-03 15:31:26 +0300

    Added Huffman coding

--- Shannon-Fano.py
+++ Shannon-Fano.py
@@ -19,8 +19,8 @@ def Shennon(list_):
         
 #Теоретически можно везде сделать +1 к индесу, но это менее эффективно по сравнению с этим спопобом.
     
-    symbol_code(list_[:index],'1')
-    symbol_code(list_[index:],'0')
+    symbol_code(list_[:index],'0')
+    symbol_code(list_[index:],'1')
     
     if len(list_[:index])!=1:
         Shennon(list_[:index])
@@ -28,14 +28,28 @@ def Shennon(list_):
     if len(list_[index:])!=1:
         Shennon(list_[index:])
 def Huffman(list_):
+    while len(list_)>1:
+        two_elem = list_.pop()
+        one_elem = list_.pop()
+        list_.append(((one_elem[0],two_elem[0]),round(one_elem[1]+two_elem[1],4)))
+        list_.sort(key = lambda x: -x[1])
+    encode_tree(list_[0][0],'')
     
-    return
+def encode_tree(tuple_,sym):
+    if isinstance(tuple_[0],str):
+        symbol_code([[tuple_[0]]],sym+'0')
+    else:
+        encode_tree(tuple_[0],sym+'0')
+    if isinstance(tuple_[1],str):
+        symbol_code([[tuple_[1]]],sym+'1')
+    else:
+        encode_tree(tuple_[1],sym+'1')
 def Choice_and_output():
     choice = input('1-Шеннон-Фано\n2-Хаффман\n')
     if choice=='1':
-        Shennon(list_frequ)
+        Shennon(list_frequ.copy())
     elif choice=='2':
-        Huffman()
+        Huffman(list_frequ.copy())
     else:
         print('Так и не выбрали как кодировать')
         return
@@ -45,6 +59,7 @@ def Choice_and_output():
     for i in list_frequ:
         print('|',i[0],' '*(5-len(str(i[0]))),'|',i[1],' '*(10-len(str(i[1]))),'|',Code[i[0]],' '*(10-len(str(Code[i[0]]))),'|')
         print('|','_'*6,'|','_'*11,'|','_'*11,'|')
+        
     #print('Список типа: "символ" - "частота появления"\n',list_frequ)
     #print('Словарь типа: "символ" - "код"\n',Code)
     print('Закодированное сообщение:\n',''.join([Code[i] if i!='\n' else i+' ' for j in input_ for i in j]))
